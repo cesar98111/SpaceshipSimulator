ae6d2bc8002a9e1fc3026b90a9896396c78f99b7
cesar jesus leon martin <cesilius98@gmail.com>
2022-04-20 09:45:42 +0200

    Feature: añadido metodo load

--- src/Cargo.java
+++ src/Cargo.java
@@ -1,9 +1,18 @@
 public class Cargo extends SpaceShip{
     int capacidadDeCarga;
+    int carga;
     public Cargo(String nombre,String matriculaGalactica,float aceleracion,int capacidadDeCarga)
     {
         super(nombre,matriculaGalactica,aceleracion);
         this.capacidadDeCarga=capacidadDeCarga;
+
+    }
+    public void load(){
+        if (capacidadDeCarga<carga){
+            carga++;
+        }else{
+            System.out.println("capacidad de transporte al tope");
+        }
 
     }
 
